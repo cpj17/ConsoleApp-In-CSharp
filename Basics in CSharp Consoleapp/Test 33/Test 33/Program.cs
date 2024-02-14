d14e34395f68fe70fbdfce05b9c8635c55dfd6e7
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 17:58:18 +0530

    Add files via upload

---
+++ Basics in CSharp Consoleapp/Test 33/Test 33/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String Split Methods
+            Console.WriteLine("Method 1 char split w/o count");
+            string str = "This is, a string, for testing, string split method";
+            Console.WriteLine("String : "+str+"\n");
+            string[] txt = str.Split(',');
+
+            foreach (string item in txt)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine("\nMethod 2 char split w/ count");
+            string str2 = "This is, a string, for testing, string, split method";
+            Console.WriteLine("String : " + str2 + "\n");
+            char[] sep2 = { ',',',' };
+            string[] txt2 = str2.Split(sep2,3, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string item in txt2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nMethod 3 string split");
+            string str3 = "This is, a string, for testing, string split method";
+            Console.WriteLine("String : " + str3 + "\n");
+            string[] sep3 = {"g,","for"};
+            string[] txt3 = str.Split(sep3, 3, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string item in txt3)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Basics in CSharp Consoleapp/Test 34/Test 34/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Remove a Particular From a String
+            string str = "This is a string";
+            Console.WriteLine("String : "+str);
+            Console.WriteLine("Enter which letter do you want to remove...");
+            var key = Console.ReadLine();
+            Console.WriteLine(str.Remove(str.IndexOf(key),1));
+        }
+    }
+}
