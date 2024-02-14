b5be98d68bd03e9636fe6a5318253f532f754169
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 16:45:12 +0530

    Add files via upload

---
+++ Basics in CSharp Consoleapp/Test 3/Test 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basics in CSharp Consoleapp/Test 4/Test 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
