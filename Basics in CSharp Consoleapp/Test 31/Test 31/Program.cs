4eafe92edb8381ceb6a881b030db14087a7990b4
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 17:53:42 +0530

    Add files via upload

---
+++ Basics in CSharp Consoleapp/Test 31/Test 31/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type casting int to double
+            Console.WriteLine("Type casting int to double");
+            var valInt = 17;
+            Console.WriteLine("Before conversion...");
+            Console.WriteLine("valInt = "+valInt+"  "+valInt.GetType());
+            Console.WriteLine("After conversion...");
+            double valDouble = valInt;
+            Console.WriteLine("valDouble = "+valDouble+"  "+valDouble.GetType());
+
+            //Type Casting double to int
+            Console.WriteLine("\nType Casting double to int");
+            var valDouble3 = 17.45;
+            Console.WriteLine("Before Conversion\nvalDouble = "+valDouble3+"  "+valDouble3.GetType());
+            var valInt2 = Convert.ToInt32(valDouble3);
+            Console.WriteLine("After Conversion\nvalint = " + valInt2 + "  " + valInt2.GetType());
+
+            //Type casting float to double
+            Console.WriteLine("\nType casting float to double");
+            var n = 17.6;
+            var valFloat = Convert.ToDecimal(n);
+            Console.WriteLine("Before Conversion");
+            Console.WriteLine("valFloat = " + valFloat + "  " + valFloat.GetType());
+            Console.WriteLine("After Conversion");
+            var valDouble2 = (double) valFloat;
+            Console.WriteLine("valDouble = " + valDouble2 + "  " + valDouble2.GetType());
+            Console.WriteLine();
+
+            //Type Casting int to string
+            Console.WriteLine("\nType casting int to string");
+            var valInt3 = 35;
+            Console.WriteLine("Before Conversion\nvalInt = " + valInt3 + "  " + valInt3.GetType());
+            var str = Convert.ToString(valInt3);
+            Console.WriteLine("After Conversion\nstr = " + str + "  " + str.GetType());
+
+            //Date Time Conversion
+            Console.WriteLine("\nDateTime Conversion");
+            var dateString = "Sat Apr 17, 2021";
+            var convertedDate = Convert.ToDateTime(DateTime.Parse(dateString));
+            Console.WriteLine(convertedDate+"  "+convertedDate.GetType());
+        }
+    }
+}
---
+++ Basics in CSharp Consoleapp/Test 32/Test 32/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_32
+{
+    class Program
+    {
+        static int myMethod(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+        static double myMethod(double val1, double val2)
+        {
+            return val1 + val2;
+        }
+        static string myMethod(string val1, string val2)
+        {
+            return val1 + val2;
+        }
+
+        static bool isNumber(string str)
+        {
+            bool flag = true;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(char.IsNumber(str[i]) == false)
+                    flag = false;
+            }
+            return flag;
+        }
+
+        static void Main(string[] args)
+        {
+            //Method Overloading
+            Console.WriteLine("Method Overloading\n");
+            Console.WriteLine("Enter Value 1 : ");
+            var val1 = Console.ReadLine();
+            Console.WriteLine("Enter Value 2 : ");
+            var val2 = Console.ReadLine();
+
+            bool v1Flag, v2Flag;
+            //To check input having dot or not
+            if (val1.Contains("."))
+            {
+                //Here remove dot and check the input numeric or not
+                var val1DotRemoved = val1.Remove(val1.IndexOf("."), 1);
+                v1Flag = isNumber(val1DotRemoved);
+            }
+            else
+                v1Flag = isNumber(val1);
+
+            if(val2.Contains("."))
+            {
+                var val2DotRemoved = val2.Remove(val2.IndexOf("."), 1);
+                v2Flag = isNumber(val2DotRemoved);
+            }
+            else
+                v2Flag = isNumber(val2);
+            
+            
+            if (v1Flag == true && v2Flag == true)
+            {
+                Console.WriteLine(myMethod(Convert.ToDouble(val1),Convert.ToDouble(val2)));
+            }
+            else
+               Console.WriteLine(myMethod(val1, val2));
+        }
+    }
+}
