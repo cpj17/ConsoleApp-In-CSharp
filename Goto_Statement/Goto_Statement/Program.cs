d578d72c5603ee14a01c6b52edf78d7efaa43144
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 13:16:43 +0530

    Add files via upload

---
+++ Goto_Statement/Goto_Statement/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Goto_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 0;
+        Loop:
+            val += 1;
+            Console.WriteLine("In Loop Statement " + val);
+
+            if (val <= 10)
+            {
+                goto Loop;
+            }
+            Console.ReadKey();
+        }
+    }
+}
