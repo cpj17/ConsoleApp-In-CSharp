a1c11ed66593b2ec5529407a932ae9d9a1515e69
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 13:32:43 +0530

    Add files via upload

---
+++ ReadDataFromInternet/ReadDataFromInternet/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ReadDataFromInternet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = File.ReadAllText("http://cpj.epizy.com/text.txt");
+
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://docs.google.com/spreadsheets/d/1Rcy4f3zcb1L7dmuZ-daUj7jEY1k0pDf4Ngh3kdM9cxw/gviz/tq?tqx=out:json&tq&gid=+gid");
+            StreamReader reader = new StreamReader(stream);
+            string content = reader.ReadToEnd();
+
+
+            String[] spearator = { "rows"};
+            Int32 count = 2;
+
+            String[] strlist = content.Split(spearator, count, StringSplitOptions.RemoveEmptyEntries);
+            string[] list = strlist[1].Split(',');
+            string str = list[1];
+            string[] l2 = str.Split('[');
+            string s2 = l2[1].Remove(l2[1].Length - 3, 3);
+
+            //Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(str);
+            #region cmts
+            //var webRequest = WebRequest.Create(@"http://cpj.epizy.com/index.html");
+
+            //using (var response = webRequest.GetResponse())
+            //using (var content = response.GetResponseStream())
+            //using (var reader = new StreamReader(content))
+            //{
+            //    var strContent = reader.ReadToEnd();
+            //}
+
+            //WebClient webClient = new WebClient();
+            //webClient.DownloadFile("https://docs.google.com/spreadsheets/d/1Rcy4f3zcb1L7dmuZ-daUj7jEY1k0pDf4Ngh3kdM9cxw/gviz/tq?tqx=out:json&tq&gid=+gid", @"D:\myfile.txt");
+            #endregion
+
+        }
+    }
+
+    public class C
+    {
+        public string v { get; set; }
+    }
+
+    public class Root
+    {
+        public List<C> c { get; set; }
+    }
+}
---
+++ ReadJsonFromLink/ReadJsonFromLink/Program.cs
@@ -0,0 +1,31 @@
+﻿string url = "https://jsonformatter.org/db74aa"; // Replace with the URL of your JSON data source
+
+try
+{
+    string json = await GetJsonAsync(url);
+    // Now you can parse the JSON data as needed
+    Console.WriteLine(json);
+
+    Console.Read();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
+
+static async Task<string> GetJsonAsync(string url)
+{
+    using (HttpClient httpClient = new HttpClient())
+    {
+        HttpResponseMessage response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            throw new Exception($"Failed to fetch data. Status code: {response.StatusCode}");
+        }
+    }
+}
