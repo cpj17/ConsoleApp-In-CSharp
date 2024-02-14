fa2fc0e02a892ebfa105ecfc7f00a771f54f8bac
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 13:02:54 +0530

    Add files via upload

---
+++ DeleteTempFolderFiles/DeleteTempFolderFiles/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace DeleteTempFolderFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Made By CPJ";
+            string tempFolderPath = Path.GetTempPath(); // Get the path of the temp folder
+            //Console.WriteLine("Temp Folder Path: " + tempFolderPath);
+
+            int totalFiles = Directory.GetFiles(tempFolderPath).Length;
+            int totalFolders = Directory.GetDirectories(tempFolderPath).Length;
+            int deletedFiles = 0;
+            int deletedFolders = 0;
+            DateTime start = DateTime.Now;
+            UpdateTimer(start);
+            // Delete all files in the temp folder
+            foreach (string filePath in Directory.GetFiles(tempFolderPath))
+            {
+                try
+                {
+                    UpdateTimer(start);
+                    File.Delete(filePath);
+                    deletedFiles++;
+                    UpdateProgress(totalFiles, totalFolders, deletedFiles, deletedFolders, start);
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error deleting file: " + ex.Message);
+                }
+            }
+
+            // Delete all folders in the temp folder
+            foreach (string folderPath in Directory.GetDirectories(tempFolderPath))
+            {
+                try
+                {
+                    UpdateTimer(start);
+                    Directory.Delete(folderPath, true); // Recursively delete all files and subfolders
+                    deletedFolders++;
+                    UpdateProgress(totalFiles, totalFolders, deletedFiles, deletedFolders, start);
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error deleting folder: " + ex.Message);
+                }
+            }
+            UpdateTimer(start);
+            UpdateProgress(totalFiles, totalFolders, totalFiles, totalFolders, start);
+            Console.WriteLine();
+            Console.WriteLine("Process Done.");
+            Console.WriteLine();
+            Console.WriteLine("This application is created by CPJ. Thanks for using this app");
+            Console.Write("Press enter key to exit.");
+            Console.ReadLine();
+        }
+
+        static void UpdateProgress(int totalFiles, int totalFolders, int deletedFiles, int deletedFolders, DateTime start)
+        {
+            double totalItems = totalFiles + totalFolders;
+            double deletedItems = deletedFiles + deletedFolders;
+            int progressPercentage = (int)Math.Round((deletedItems / totalItems) * 100);
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write("Progress \t\t: {0}%", progressPercentage);
+
+            //Console.SetCursorPosition(0, 1);
+            //Console.Write("Time elapsed \t\t: {0}", DateTime.Now - start);
+            UpdateTimer(start);
+        }
+
+        private static void UpdateTimer(DateTime start)
+        {
+            Console.SetCursorPosition(0, 1);
+            Console.Write("Time elapsed \t\t: {0}", DateTime.Now - start);
+        }
+    }
+}
---
+++ DistinctRowFromDT/DistinctRowFromDT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+namespace DistinctRowFromDT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("col1");
+            dt.Columns.Add("col2");
+            dt.Columns.Add("col3");
+
+            dt.Rows.Add("c11", "c12", "c13");
+            dt.Rows.Add("c21", "c22", "c23");
+            dt.Rows.Add("c11", "c42", "c13");
+            dt.Rows.Add("c31", "c32", "c33");
+
+            DataView objView = new DataView(dt);
+            DataTable dt2 = new DataTable();
+            //dt2 = objView.ToTable("nt", true, new string[] { "col1", "col3" });
+            dt2 = objView.ToTable("nt", true, "col1", "col3");
+
+            DataSet ds = new DataSet();
+            ds.Tables.Add(dt2);
+
+        }
+    }
+}
