9fc5ccc8396ca9ffae13fcf85fdf2492a58fc06e
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 13:36:49 +0530

    Add files via upload

---
+++ Sleep/Sleep/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Going to sleep…");
+            Thread.Sleep(1000);
+            Console.WriteLine("Woke up after 1 sec!");
+
+            //Method 2
+            //Console.WriteLine("Going to sleep…");
+            //Stopwatch stopwatch = Stopwatch.StartNew();
+            //while (stopwatch.ElapsedMilliseconds < 1000) ;
+            //Console.WriteLine("Woke up after 1 sec!");
+
+            Console.Read();
+        }
+    }
+}
---
+++ SortInDT/SortInDT/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortInDT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("col1");
+            dt.Columns.Add("col2");
+            dt.Columns.Add("col3");
+
+            dt.Rows.Add("11", "12", "13");
+            dt.Rows.Add("21", "22", "23");
+            dt.Rows.Add("31", "32", "33");
+            dt.Rows.Add("41", "42", "43");
+
+            DataView dv = new DataView();
+            dv = dt.DefaultView;
+            dv.Sort = "col1 DESC";
+            dt = dv.ToTable();
+
+        }
+    }
+}
