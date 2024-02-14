a93313953f672b8d7ad7b56b8146a83c771f3ad5
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 13:30:50 +0530

    Add files via upload

---
+++ PrimaryKeyInDT/PrimaryKeyInDT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+namespace PrimaryKeyInDT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            DataTable dt2 = new DataTable();
+
+            dt.Columns.Add("id");
+            dt.Columns.Add("name");
+
+            dt.Rows.Add("1", "aa");
+            dt.Rows.Add("2", "bb");
+
+            dt2.Columns.Add("id");
+            dt2.Columns.Add("dept");
+
+            dt2.Rows.Add("1", "xx");
+            dt2.Rows.Add("2", "yy");
+
+            dt2.PrimaryKey = new DataColumn[] { dt2.Columns["id"] };
+
+            dt.Merge(dt2);
+        }
+    }
+}
