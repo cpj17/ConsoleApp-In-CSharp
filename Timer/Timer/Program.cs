ca3f00ffe1d733999c5d29bf34d5e1e56020abe3
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 13:45:00 +0530

    Add files via upload

---
+++ Timer/Timer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Timer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec = 1 * 1000;
+
+            var timer = new Timer(TimerMethod, null, 0, sec);
+            Console.ReadKey();
+        }
+        public static void TimerMethod(object e)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
