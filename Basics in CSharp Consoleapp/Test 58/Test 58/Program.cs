bd5723f2c5cee07fa43b0a61aea4eb6adaf649f0
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 20:06:55 +0530

    Add files via upload

---
+++ Basics in CSharp Consoleapp/Test 58/Test 58/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Test_58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DataTable Creation
+            DataTable dtStudData1 = new DataTable("Student Data 1");
+
+            //Data Column Creation And Added
+            dtStudData1.Columns.Add("Id",typeof(Int32));
+            dtStudData1.Columns.Add("Name", typeof(string));
+            dtStudData1.Columns.Add("Dept", typeof(string));
+
+            //Data Row And Values Added to Data Table
+            dtStudData1.Rows.Add(101,"Praveen","CSE");
+            dtStudData1.Rows.Add(102, "Jeeva", "ECE");
+            dtStudData1.Rows.Add(103, "CPJ", "IT");
+
+            //Data Table 2 For Other Student Details
+            DataTable dtStudData2 = new DataTable("Student Data Others");
+
+            //DataTable 2 Columns
+            dtStudData2.Columns.Add("Id",typeof(Int32));
+            dtStudData2.Columns.Add("Place", typeof(string));
+            dtStudData2.Columns.Add("District", typeof(string));
+
+            //DataTable 2 Rows
+            dtStudData2.Rows.Add(101,"Valai","NKL");
+            dtStudData2.Rows.Add(102, "Rasipuram", "KAR");
+            dtStudData2.Rows.Add(103, "Pachal", "CHE");
+            //dtStudData2.Rows.Add(104, "Chathiram", "SLM");
+
+            //DataSet Creation
+            DataSet dsStud = new DataSet();
+            dsStud.Tables.Add(dtStudData1);
+            dsStud.Tables.Add(dtStudData2);
+
+            //Relation
+            DataRelation drStud = new DataRelation("", dtStudData1.Columns[0], dtStudData2.Columns[0]);
+            dsStud.Relations.Add(drStud);
+
+            DataRow parentRow;
+            foreach (DataRow childRow in dtStudData2.Rows)
+            {
+                parentRow = childRow.GetParentRow(drStud);
+                Console.WriteLine("Parent Row = "+parentRow[1]);
+                Console.WriteLine("Child Row = " + childRow[1]);
+            }
+
+        }
+    }
+}
---
+++ Basics in CSharp Consoleapp/Test 59/Test 59/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Xml;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Test_59
+{
+    class Program
+    {
+        static Int32 checkUsername(DataSet objDataSetUser, string username)
+        {
+            DataRow[] dataRow = objDataSetUser.Tables[0].Select("Username='" + username + "'");
+            Int32 count = dataRow.Count();
+            objDataSetUser.Clear();
+            return count;
+        }
+        static void createAcc(DataSet objDataSetUser)
+        {
+            objDataSetUser.ReadXml("userData.xml",XmlReadMode.Auto);
+
+            Console.WriteLine("\nEnter Your Name");
+            var name = Console.ReadLine();
+            var username = "";
+            bool usernameFlag = true;
+            while (usernameFlag)
+            {
+                Console.WriteLine("Enter UserName");
+                username = Console.ReadLine();
+                if (checkUsername(objDataSetUser, username) > 0)
+                    Console.WriteLine("Username Exists Choose new one....");
+                else
+                    usernameFlag = false;
+            }
+            
+            Console.WriteLine("Enter Your Password");
+            var password = Console.ReadLine();
+            objDataSetUser.ReadXml("userData.xml", XmlReadMode.Auto);
+            objDataSetUser.Tables[0].Rows.Add(name, username, password);
+
+            objDataSetUser.Tables[0].WriteXml("userData.xml",XmlWriteMode.WriteSchema);
+            Console.WriteLine("Account Created Successfully");
+            objDataSetUser.Clear();
+
+        }
+        static void login(DataSet objDataSetUser)
+        {
+            var username = "";
+            bool usernameFlag = true;
+            while (usernameFlag)
+            {
+                Console.WriteLine("Enter UserName");
+                username = Console.ReadLine();
+                
+                if (checkUsername(objDataSetUser, username) == 0)
+                    Console.WriteLine("Invalid Username....");
+                else
+                    usernameFlag = false;
+            }
+
+            Console.WriteLine("Enter Your Password");
+            var password = Console.ReadLine();
+
+            objDataSetUser.ReadXml("userData.xml",XmlReadMode.Auto);
+            DataRow[] objDataRowUser = objDataSetUser.Tables[0].Select("Username='"+ username +"'");
+            if (objDataRowUser[0]["Password"].ToString() == password)
+            {
+                objDataSetUser.ReadXml("userLog.xml");
+                DateTime currentTime = DateTime.Now;
+                objDataSetUser.Tables[1].Rows.Add(username, currentTime);
+
+                //Console.WriteLine(objDataSetUser.Tables.Count);
+                objDataSetUser.Tables[1].WriteXml("userLog.xml", XmlWriteMode.WriteSchema);
+
+                Console.WriteLine("Welcome " + objDataRowUser[0]["Name"]);
+                Console.WriteLine("You're Logged In");
+                objDataSetUser.Clear();
+            }
+            else
+                Console.WriteLine("Invalid Password");
+        }
+        static void loginHistory(DataSet objDataSetUser)
+        {
+            //DataRow[] dataRow = objDataSetUser.Tables[1].Select("Username='" + username + "'");
+            //Console.WriteLine(dataRow.Count());
+
+            var username = "";
+            bool usernameFlag = true;
+            while (usernameFlag)
+            {
+                Console.WriteLine("Enter UserName");
+                username = Console.ReadLine();
+
+                if (checkUsername(objDataSetUser, username) == 0)
+                    Console.WriteLine("Invalid Username....");
+                else
+                    usernameFlag = false;
+            }
+
+            Console.WriteLine("Enter Your Password");
+            var password = Console.ReadLine();
+
+            objDataSetUser.ReadXml("userData.xml", XmlReadMode.Auto);
+            objDataSetUser.ReadXml("userLog.xml", XmlReadMode.Auto);
+
+            DataRow[] objDataRowUser = objDataSetUser.Tables[0].Select("Username='" + username + "'");
+            if (objDataRowUser[0]["Password"].ToString() == password)
+            {
+                DataRow[] objDataRowUserLog = objDataSetUser.Tables[1].Select("Username='" + username + "'");
+                Console.WriteLine("\nTotal Logins : "+objDataRowUserLog.Count());
+
+                DataRelation objDataRelation = new DataRelation("User_DataRelation", objDataSetUser.Tables[0].Columns[1], objDataSetUser.Tables[1].Columns[0]);
+                
+                objDataSetUser.Relations.Add(objDataRelation);
+                
+                DataRow parentRow;
+                foreach (DataRow childRow in objDataSetUser.Tables[1].Rows)
+                {
+                    parentRow = childRow.GetParentRow(objDataRelation);
+                    //Console.WriteLine(childRow[1]);
+                    if (childRow[0].ToString() == username)
+                        Console.WriteLine("Time Stamp = "+childRow[1]);
+                }
+            }
+            else
+                Console.WriteLine("Invalid Password");
+            objDataSetUser.Relations.Clear();
+            objDataSetUser.Clear();
+        }
+        static void Main(string[] args)
+        {
+            DataSet objDataSetUser = new DataSet("User_DataSet");
+
+            //Add Table To DataSet
+            objDataSetUser.ReadXml("userData.xml");
+
+            var choice = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\n1. Create Account");
+                    Console.WriteLine("2. Login");
+                    Console.WriteLine("3. View LogIn History");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("Enter your chioce");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            createAcc(objDataSetUser);
+                            break;
+                        case 2:
+                            login(objDataSetUser);
+                            break;
+                        case 3:
+                            loginHistory(objDataSetUser);
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Enter Valid Choice");
+                            break;
+                    }
+                }
+                catch(Exception e)
+                {
+                   Console.WriteLine("Error : "+e.Message);
+                }
+            } while (choice != 4);
+        }
+    }
+}
