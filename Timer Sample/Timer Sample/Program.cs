f7e8eac0f2b7080b851b303b300795e0401c8034
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 13:46:45 +0530

    Add files via upload

---
+++ Timer Sample/Timer Sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Timers;
+
+namespace Timer_Sample
+{
+    class Program
+    {
+        static Timer timer = new Timer();
+
+        static void Main(string[] args)
+        {
+            timer.Interval = 1000;
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+            Console.Read();
+        }
+
+        private static void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
