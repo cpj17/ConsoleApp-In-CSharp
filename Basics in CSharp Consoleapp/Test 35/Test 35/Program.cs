6552fa86a425dbba37b06a27c5fdf215077b0294
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 18:00:01 +0530

    Add files via upload

---
+++ Basics in CSharp Consoleapp/Test 35/Test 35/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is a string";
+            Console.WriteLine("Given String : "+str+"\nWhich Letters do you want to remove....");
+            string key = Console.ReadLine();
+            string[] sep = { key };
+            string[] txt = str.Split(sep,StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in txt)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Basics in CSharp Consoleapp/Test 36/Test 36/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sorting
+            int[] arrInt = new int[] { 2, 5, 7, 3, 1,5 };
+            Console.WriteLine("Given Elements");
+            foreach (int item in arrInt)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("\nAssending Order Sort in integer");
+            Array.Sort(arrInt);
+            foreach (int item in arrInt)
+            {
+                Console.Write(item+"  ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Desending Order Sort in integer");
+            Array.Reverse(arrInt);
+            foreach (int item in arrInt)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine("\n");
+            
+            Console.WriteLine("Given Elements");
+            string[] arrStr = new string[] { "C", "F", "B", "E", "A", "D" };
+            foreach (string item in arrStr)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Assending Order Sort in alphabets");
+            Array.Sort(arrStr);
+            foreach (string item in arrStr)
+            {
+                Console.Write(item+"  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Desending Order Sort in alphabets");
+            Array.Reverse(arrStr);
+            foreach (string item in arrStr)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
