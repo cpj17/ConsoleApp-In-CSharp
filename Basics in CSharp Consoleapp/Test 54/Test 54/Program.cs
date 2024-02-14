db630a942778a91af1a0268d1439306e2d73b4cf
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-14 19:58:12 +0530

    Add files via upload

---
+++ Basics in CSharp Consoleapp/Test 54/Test 54/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace Test_54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Mark1", typeof(Int32));
+            dt.Columns.Add("Mark2", typeof(Int32));
+            dt.TableName = "Table";
+
+            //Add rows
+            DataRow dr = dt.NewRow();
+            dr["Mark1"] = 30;
+            dr["Mark2"] = 50;
+            dt.Rows.Add(dr);
+
+            DataColumn col = new DataColumn();
+            col.DataType = typeof(Int32);
+            col.Expression = "Mark1 + Mark2";
+            col.ColumnName = "Total";
+            dt.Columns.Add(col);
+            dt.Compute("Sum(Total)","");
+        }
+    }
+}
---
+++ Basics in CSharp Consoleapp/Test 55/Test 55/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Mark1", typeof(Int32));
+            dt.Columns.Add("Mark2", typeof(Int32));
+            dt.TableName = "Table";
+
+            //Add rows
+            DataRow dr = dt.NewRow();
+            dr["Mark1"] = 30;
+            dr["Mark2"] = 50;
+            dt.Rows.Add(dr);
+
+            //dt.RejectChanges();
+            Console.WriteLine(dt.Rows[0].RowState);
+            dt.AcceptChanges();
+            Console.WriteLine(dt.Rows[0].RowState);
+            DataRow[] row = dt.Select("Mark1=30");
+            row[0]["Mark1"] = 100;
+            dt.Rows.Add(55,66);
+            Console.WriteLine(dt.Rows[0].RowState);
+            
+        }
+    }
+}
