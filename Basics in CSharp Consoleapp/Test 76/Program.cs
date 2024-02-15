e12a986d5ffc845f78d2e17215c3fa7e7cce0b61
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-15 08:27:24 +0530

    Add files via upload

---
+++ Basics in CSharp Consoleapp/Test 76/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+
+namespace Test_76
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet ds1 = new DataSet();
+            DataSet ds2 = new DataSet();
+            DataSet ds3 = new DataSet();
+
+            DataTable dt1 = new DataTable();
+            DataTable dt2 = new DataTable();
+
+            dt1.Columns.Add("id", typeof(string));
+            dt1.Columns.Add("name", typeof(string));
+            dt1.Columns.Add("dept", typeof(string));
+
+            dt1.Rows.Add("101","aa","cse");
+            dt1.Rows.Add("103","bb","ece");
+            dt1.Rows.Add("103","cc","eee");
+
+            dt2.Columns.Add("id", typeof(string));
+            dt2.Columns.Add("district", typeof(string));
+            dt2.Columns.Add("state", typeof(string));
+
+            dt2.Rows.Add("101", "nkl", "tn");
+            dt2.Rows.Add("103", "slm", "tn");
+            dt2.Rows.Add("103", "kur", "tn");
+
+            ds1.Tables.Add(dt1);
+            ds2.Tables.Add(dt2);
+
+            ds1.Merge(ds2);
+        }
+    }
+}
---
+++ Basics in CSharp Consoleapp/Test 77/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Collections;
+using System.Globalization;
+
+namespace Test_77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateValue;
+            string dateString = "2021-05-25T18:36:32+05:30";
+
+            try
+            {
+                dateValue = DateTime.Parse(dateString, new CultureInfo("fr-FR", true));
+                Console.WriteLine("'{0}' converted to {1}.", dateString, dateValue);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Unable to convert '{0}'.", dateString);
+            }
+        }
+    }
+}
