dbce6f8da1eb90042b68c3d8c9d0c8497e12d7fb
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-15 08:24:15 +0530

    Add files via upload

---
+++ Basics in CSharp Consoleapp/Test75/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Test75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt1;
+            DateTime dt2;
+            DateTime dt3;
+            string str;
+
+            dt1 = DateTime.Now;
+            Thread.Sleep(1030);
+            dt2 = DateTime.Now;
+            
+            str = dt2.Subtract(dt1).ToString();
+            
+            //DataSet objDataSetStringInfo = new DataSet();
+
+            //objDataSetStringInfo.ReadXml("D:/Logs/objDataSetStringInfo2.xml");
+            
+            //List<dynamic> objDynamicList = new List<dynamic>();
+            //objDynamicList.Add(objDataSetStringInfo);
+            //objDynamicList.Add("hgjhg");
+
+            //TestFun(objDynamicList);
+        }
+
+        private static void TestFun(List<dynamic> objDynamicList)
+        {
+            DataSet ds = new DataSet();
+            ds = objDynamicList[0];
+
+            string str = objDynamicList[1];
+        }
+    }
+}
